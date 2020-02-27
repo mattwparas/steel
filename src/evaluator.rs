d2c87172461e5cde3d5e8bd1c97d816362e96168
frogbird <matthewparas2020@u.northwestern.edu>
2020-02-27 10:47:04 -0600

    cleanup

--- src/evaluator.rs
+++ src/evaluator.rs
@@ -98,7 +98,6 @@ pub fn check_length(what: &str, tokens: &[Expr], expected: usize) -> Result<()> 
 pub fn evaluate(expr: &Expr, env: &Rc<RefCell<Env>>) -> Result<RucketVal> {
     let mut env = Rc::clone(env);
     let mut expr = expr.clone();
-    //println!("evaluating expr: {}", expr);
 
     loop {
         match expr {
@@ -115,10 +114,7 @@ pub fn evaluate(expr: &Expr, env: &Rc<RefCell<Env>>) -> Result<RucketVal> {
                 Token::StringLiteral(s) => {
                     return Ok(RucketVal::StringV(s));
                 }
-                what => {
-                    stop!(UnexpectedToken => what)
-                    // return Err(RucketErr::UnexpectedToken(what.to_string()));
-                }
+                what => stop!(UnexpectedToken => what),
             },
 
             Expr::ListVal(list_of_tokens) => {
@@ -181,14 +177,11 @@ pub fn evaluate(expr: &Expr, env: &Rc<RefCell<Env>>) -> Result<RucketVal> {
                                 env = Rc::new(RefCell::new(inner_env));
                                 expr = lambda.body_exp();
                             }
-                            e => stop!(ExpectedFunction => e), // {
-                                                               //     return Err(RucketErr::ExpectedFunction(e.to_string()));
-                                                               // }
+                            e => stop!(ExpectedFunction => e),
                         },
                     }
                 } else {
                     stop!(ExpectedFunction => "Given empty list")
-                    // return Err(RucketErr::ExpectedFunction("Empty List".to_string()));
                 }
             }
         }
@@ -240,13 +233,8 @@ pub fn eval_define(list_of_tokens: &[Expr], env: Rc<RefCell<Env>>) -> Result<Rc<
         }
         // construct lambda to parse
         Expr::ListVal(list_of_identifiers) => {
-            // check_length("Define", tokens: &[Expr], expected: usize)
-
             if list_of_identifiers.is_empty() {
                 stop!(ExpectedIdentifier => "define expected an identifier, got empty list")
-                // return Err(RucketErr::ExpectedIdentifier(
-                //     "define expected an identifier, got empty list".to_string(),
-                // ));
             }
             if let Expr::Atom(Token::Identifier(s)) = &list_of_identifiers[0] {
                 // TODO: this code needs go through evaluate again
@@ -259,27 +247,14 @@ pub fn eval_define(list_of_tokens: &[Expr], env: Rc<RefCell<Env>>) -> Result<Rc<
 
                 let constructed_lambda = Expr::ListVal(fake_lambda);
 
-                // let constructed_lambda = eval_make_lambda(&fake_lambda, env)?;
-
                 let eval_body = evaluate(&constructed_lambda, &env)?;
                 env.borrow_mut().define(s.to_string(), eval_body);
                 Ok(env)
-
-            // eval_make_lambda(, env: EnvRef)
             } else {
                 stop!(ExpectedIdentifier => "Define expected identifier, got: {}", symbol);
-
-                // Err(RucketErr::ExpectedIdentifier(format!(
-                //     "Define expects identifier, got: {}",
-                //     symbol
-                // )))
             }
         }
         _ => stop!(ExpectedIdentifier => "Define expects an identifier, got: {}", symbol),
-        // Err(RucketErr::ExpectedIdentifier(format!(
-        //     "Define expects identifier, got: {}",
-        //     symbol
-        // ))),
     }
 }
 
@@ -305,21 +280,13 @@ pub fn eval_let(list_of_tokens: &[Expr], _env: &Rc<RefCell<Env>>) -> Result<Expr
                             bindings_to_check.push(binding.clone());
                             args_to_check.push(expression.clone());
                         }
-                        _ => stop!(ContractViolation => "Let requires pairs for binding"), // return Err(RucketErr::ContractViolation(
-                                                                                           //     "Let requires pairs for binding".to_string(),
-                                                                                           // ));
+                        _ => stop!(ContractViolation => "Let requires pairs for binding"),
                     },
-                    _ => stop!(BadSyntax => "Let: Missing body"), // {
-                                                                  //     return Err(RucketErr::BadSyntax("Let: Missing body".to_string()));
-                                                                  // }
+                    _ => stop!(BadSyntax => "Let: Missing body"),
                 }
             }
         }
-        _ => stop!(BadSyntax => "Let: Missing name or binding pairs"), // {
-                                                                       //     return Err(RucketErr::BadSyntax(
-                                                                       //         "Let: Missing name or binding pairs".to_string(),
-                                                                       //     ));
-                                                                       // }
+        _ => stop!(BadSyntax => "Let: Missing name or binding pairs"),
     }
 
     let mut combined = vec![Expr::ListVal(vec![
