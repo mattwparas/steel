ec120c44eda58bd5937753985b01d27a7b8e3450
frogbird <matthewparas2020@u.northwestern.edu>
2020-07-23 00:30:13 -0500

    trying to fix global mutual recursion

--- dep.rkt
+++ dep.rkt
@@ -4,17 +4,37 @@
 (define boop-boop "alex-is-the-best")
 
 
+(λ (x y) (+ 1 2 3))
+
+(defn (take lst n)
+  (defn (loop x l acc)
+    (if (= x 0)
+        acc
+        (loop (- x 1) (cdr l) (cons (car l) acc))))
+  (loop n lst (list)))
+
+
 (define (take lst n)
   (define (loop x l accum)
-    (if (or (zero? x) (null? l))
+    (if (= x 0)
         accum
-        (loop (sub1 x) (cdr l) (append accum (list (car l))))))
-  (loop n lst '()))
+        (loop (- x 1) (cdr l) (cons (car l) accum))))
+  (loop n lst (list)))
 
 
 (define take (lambda (lst n)
                (define loop (lambda (x l accum)
-                              (if (or (zero? x) (null? l))
+                              (if (or (= x 0) (null? l))
                                   accm
                                   (loop (sub1 x) (cdr l) (cons (car l) accum)))))
                (loop n lst empty)))
+
+(define take (lambda (lst n)
+               (define loop (lambda (x l accum)
+                              (if (= x 0)
+                                  accum
+                                  (loop (- x 1) (cdr l) (cons (car l) accum)))))
+               (loop n lst (list))))
+
+
+(defn (take lst n))
