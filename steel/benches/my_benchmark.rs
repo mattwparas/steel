d5aee85e0ab043993b5fdbbfef7f5dc11c71804e
frogbird <matthewparas2020@u.northwestern.edu>
2020-09-22 15:26:26 -0500

    dead code

--- steel/benches/my_benchmark.rs
+++ steel/benches/my_benchmark.rs
@@ -1,15 +1,8 @@
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
 
-// use steel::env::Env;
-// use steel::interpreter::SteelInterpreter;
-use steel::stdlib::PRELUDE;
-// use steel::vm::ArityMap;
-// use steel::vm::ConstantMap;
-// use steel::vm::ConstantTable;
 use std::rc::Rc;
-// use steel::vm::Ctx;
+use steel::stdlib::PRELUDE;
 use steel::vm::VirtualMachine;
-// use steel::PRELUDE;
 
 fn range(c: &mut Criterion) {
     let script = "(range 0 50000)";
