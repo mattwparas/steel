5406fcebf313f3c2d5f5df953a5203a13c2030ee
mattwparas <matthewparas2020@u.northwestern.edu>
2021-02-13 00:41:39 -0600

    move stuff

--- steel/src/lib.rs
+++ steel/src/lib.rs
@@ -1,42 +1,22 @@
 extern crate im_rc;
-// extern crate steel_parser;
-
-// #[macro_use]
-// extern crate lazy_static;
-
 #[macro_use]
 pub mod env;
-// pub mod evaluator;
 #[macro_use]
-// pub mod interpreter;
-
-// pub mod parser;
 pub mod core;
-pub mod steel_compiler;
-
 pub mod primitives;
-// #[macro_use]
-// pub mod repl;
+pub mod steel_compiler;
 #[macro_use]
 pub mod rerrs;
 pub mod rvals;
 pub mod stdlib;
 #[macro_use]
-// pub mod expander;
-// #[macro_use]
-// pub mod compiler;
 pub mod gc;
+pub mod contracts;
 pub mod json_vals;
 pub mod lazy_stream;
+pub mod parser;
 pub mod port;
 pub mod structs;
-
-pub mod contracts;
-pub mod parser;
-// pub mod vm;
-
-// #[cfg(test)]
-// mod test_util;
 
 pub use self::{
     gc::Gc,
--- steel/src/steel_compiler/mod.rs
+++ steel/src/steel_compiler/mod.rs
@@ -1,8 +1,5 @@
 pub mod code_generator;
-// pub mod codegen;
 pub mod compiler;
 pub mod constants;
-// pub mod expand;
-// pub mod expander;
 pub mod map;
 pub mod program;
