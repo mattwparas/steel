509381fd50b6fcfb6e4efc2d4c310f482d80dd25
mattwparas <matthewparas2020@u.northwestern.edu>
2021-02-13 20:03:31 -0600

    some extra stuff

--- steel/src/scheme/methods.rkt
+++ steel/src/scheme/methods.rkt
@@ -2,7 +2,7 @@
 
 (define-syntax def-method
   (syntax-rules ()
-    [(def-method struct-name (define (a this b ...) body ...))
+    [(def-method struct-name (define/method (a this b ...) body ...))
      (define ((datum->syntax struct-name . a) this b ...)
        (unless ((datum->syntax struct-name ?) this)
          (error! (datum->syntax struct-name . a) "method takes a value of" struct-name "given" this))
@@ -11,16 +11,8 @@
 
 (define-syntax impl
   (syntax-rules ()
-    [(impl struct-name (define (a this b ...) body ...) c ...)
+    [(impl struct-name (define/method (a this b ...) body ...) c ...)
      (begin (def-method struct-name (define (a this b ...) body ...))
             (impl struct-name c ...))]
-    [(impl struct-name (define (a this b ...) body ...))
+    [(impl struct-name (define/method (a this b ...) body ...))
      (def-method struct-name (define (a this b ...) body ...))]))
-
-
-;; (struct Apple (a b c))
-;; (impl Apple
-;;       (define (eat this number)
-;;         (+ number 25))
-;;       (define (color this number)
-;;         (+ number 50)))
