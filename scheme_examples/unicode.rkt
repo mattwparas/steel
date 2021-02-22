b699dfe3b5dc4d506328d05642ea51e47a300d81
mattwparas <matthewparas2020@u.northwestern.edu>
2021-02-21 23:44:18 -0600

    unicode example, kinda busted but hey it works for now

---
+++ scheme_examples/unicode.rkt
@@ -0,0 +1,12 @@
+;; Steel supports unicode, whether you want math symbols or just emojis
+
+(define 😁 15)
+(define 😼 25)
+(define (🔥 👨 👩)
+    (+ 👨 👩))
+
+(🔥 😁 😼)
+
+(define blagh "😂😂💯")
+
+(define 😂😂💯 100)
