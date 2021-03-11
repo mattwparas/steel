f8f95993e3f19e434fa3507a3c965a9503a09d57
mattwparas <matthewparas2020@u.northwestern.edu>
2021-03-11 15:49:30 -0600

    doing ctrl-c interrupts

--- steel_repl/src/repl.rs
+++ steel_repl/src/repl.rs
@@ -1,5 +1,8 @@
 extern crate rustyline;
 use colored::*;
+
+use std::sync::mpsc::{channel, Receiver, Sender};
+
 use rustyline::error::ReadlineError;
 use rustyline::highlight::{Highlighter, MatchingBracketHighlighter};
 use rustyline::validate::{
@@ -8,7 +11,10 @@ use rustyline::validate::{
 use rustyline::Editor;
 use rustyline::{hint::Hinter, CompletionType, Context};
 use rustyline_derive::Helper;
-use std::path::Path;
+use std::{
+    path::{Path, PathBuf},
+    sync::{Arc, Mutex},
+};
 use steel::rvals::SteelVal;
 
 use rustyline::completion::Completer;
@@ -22,7 +28,17 @@ use steel_vm::engine::Engine;
 use std::io::Read;
 use steel::stdlib::{CONTRACTS, PRELUDE};
 
+use once_cell::sync::Lazy;
 use std::time::Instant;
+
+pub(crate) static INTERRUPT_CHANNEL: Lazy<(Arc<Mutex<Sender<()>>>, Arc<Mutex<Receiver<()>>>)> =
+    Lazy::new(|| {
+        let (sender, receiver) = channel::<()>();
+        (
+            Arc::new(Mutex::new(sender)),
+            (Arc::new(Mutex::new(receiver))),
+        )
+    });
 
 impl Completer for RustylineHelper {
     type Candidate = Pair;
@@ -92,6 +108,103 @@ fn display_help() {
     );
 }
 
+// enum Interrupt {
+//     Interrupted,
+// }
+
+async fn finish_load_or_interrupt(
+    vm: Arc<Mutex<Engine>>,
+    exprs: String,
+    path: PathBuf,
+    optimizations: bool,
+) {
+    tokio::spawn(async move {
+        // println!("Installed Ctrl-C handler");
+        tokio::signal::ctrl_c()
+            .await
+            .unwrap_or_else(|err| panic!("Error installing signal handler: {}", err));
+
+        let _ = Arc::clone(&INTERRUPT_CHANNEL.0).lock().unwrap().send(());
+    });
+
+    let local = tokio::task::LocalSet::new();
+    local.spawn_local(async move {
+        let file_name = path.to_str().unwrap().to_string();
+
+        let res = if optimizations {
+            vm.lock().unwrap().run_with_path(exprs.as_str(), path)
+        } else {
+            vm.lock().unwrap().run_with_path(exprs.as_str(), path)
+            // vm.parse_and_execute_without_optimizations(exprs.as_str())
+        };
+
+        match res {
+            Ok(r) => r.iter().for_each(|x| match x {
+                SteelVal::Void => {}
+                _ => println!("{} {}", "=>".bright_blue().bold(), x),
+            }),
+            Err(e) => {
+                e.emit_result(file_name.as_str(), exprs.as_str());
+                eprintln!("{}", e.to_string().bright_red());
+            }
+        }
+    });
+
+    local.await;
+}
+
+async fn finish_or_interrupt(
+    vm: Arc<Mutex<Engine>>,
+    line: String,
+    print_time: bool,
+    optimizations: bool,
+) {
+    // let (sender, receiver) = oneshot::channel();
+
+    tokio::spawn(async move {
+        // println!("Installed Ctrl-C handler");
+        tokio::signal::ctrl_c()
+            .await
+            .unwrap_or_else(|err| panic!("Error installing signal handler: {}", err));
+
+        let _ = Arc::clone(&INTERRUPT_CHANNEL.0).lock().unwrap().send(());
+
+        // let _ = sender.send(());
+    });
+
+    let local = tokio::task::LocalSet::new();
+    local.spawn_local(async move {
+        let now = Instant::now();
+
+        // println!("running...");
+
+        let res = if optimizations {
+            vm.lock().unwrap().parse_and_execute(&line)
+        } else {
+            vm.lock()
+                .unwrap()
+                .parse_and_execute_without_optimizations(&line)
+        };
+
+        match res {
+            Ok(r) => r.iter().for_each(|x| match x {
+                SteelVal::Void => {}
+                _ => println!("{} {}", "=>".bright_blue().bold(), x),
+            }),
+            Err(e) => {
+                e.emit_result("repl.stl", line.as_str());
+                // eprintln!("{}", e.to_string().bright_red());
+            }
+        }
+
+        if print_time {
+            println!("Time taken: {:?}", now.elapsed());
+        }
+    });
+
+    local.await;
+}
+
 /// Entire point for the repl
 /// Automatically adds the prelude and contracts for the core library
 pub fn repl_base(mut vm: Engine) -> std::io::Result<()> {
@@ -140,6 +253,22 @@ pub fn repl_base(mut vm: Engine) -> std::io::Result<()> {
     let mut print_time = false;
     let mut optimizations = false;
 
+    // Create the runtime
+    // We really only need this for interrupts
+    let rt = tokio::runtime::Runtime::new()?;
+
+    vm.on_progress(|x| {
+        if x % 1000 == 0 {
+            match Arc::clone(&INTERRUPT_CHANNEL.1).lock().unwrap().try_recv() {
+                Ok(_) => return false,
+                _ => {}
+            }
+        }
+        true
+    });
+
+    let vm = Arc::new(Mutex::new(vm));
+
     loop {
         let readline = rl.readline(&prompt);
         match readline {
@@ -184,51 +313,59 @@ pub fn repl_base(mut vm: Engine) -> std::io::Result<()> {
                         let mut exprs = String::new();
                         file.read_to_string(&mut exprs)?;
 
-                        let res = if optimizations {
-                            vm.run_with_path(exprs.as_str(), path.to_path_buf())
-                        } else {
-                            vm.run_with_path(exprs.as_str(), path.to_path_buf())
-                            // vm.parse_and_execute_without_optimizations(exprs.as_str())
-                        };
+                        // let file_name = path.to_str().unwrap().to_string();
 
-                        match res {
-                            Ok(r) => r.iter().for_each(|x| match x {
-                                SteelVal::Void => {}
-                                _ => println!("{} {}", "=>".bright_blue().bold(), x),
-                            }),
-                            Err(e) => {
-                                e.emit_result("repl.stl", exprs.as_str());
-                                eprintln!("{}", e.to_string().bright_red());
-                            }
-                        }
+                        let action = finish_load_or_interrupt(
+                            Arc::clone(&vm),
+                            exprs,
+                            path.to_path_buf(),
+                            optimizations,
+                        );
+
+                        rt.block_on(action)
+
+                        // let res = if optimizations {
+                        //     vm.lock()
+                        //         .unwrap()
+                        //         .run_with_path(exprs.as_str(), path.to_path_buf())
+                        // } else {
+                        //     vm.lock()
+                        //         .unwrap()
+                        //         .run_with_path(exprs.as_str(), path.to_path_buf())
+                        //     // vm.parse_and_execute_without_optimizations(exprs.as_str())
+                        // };
+
+                        // match res {
+                        //     Ok(r) => r.iter().for_each(|x| match x {
+                        //         SteelVal::Void => {}
+                        //         _ => println!("{} {}", "=>".bright_blue().bold(), x),
+                        //     }),
+                        //     Err(e) => {
+                        //         e.emit_result(file_name.as_str(), exprs.as_str());
+                        //         eprintln!("{}", e.to_string().bright_red());
+                        //     }
+                        // }
+
+                        // let action = finish_load_or_interrupt(
+                        //     Arc::clone(&vm),
+                        //     exprs,
+                        //     path.to_path_buf(),
+                        //     optimizations,
+                        // );
+
+                        // match rt.block_on(action) {
+                        //     Ok(_) => {}
+                        //     Err(_) => {
+                        //         println!("INTERRUPT")
+                        //     }
+                        // }
                     }
                     _ => {
-                        // println!("Active Object Count: {:?}", steel::gc::OBJECT_COUNT);
+                        // TODO also include this for loading files
+                        let action =
+                            finish_or_interrupt(Arc::clone(&vm), line, print_time, optimizations);
 
-                        let now = Instant::now();
-
-                        let res = if optimizations {
-                            vm.parse_and_execute(&line)
-                        } else {
-                            vm.parse_and_execute_without_optimizations(&line)
-                        };
-
-                        match res {
-                            Ok(r) => r.iter().for_each(|x| match x {
-                                SteelVal::Void => {}
-                                _ => println!("{} {}", "=>".bright_blue().bold(), x),
-                            }),
-                            Err(e) => {
-                                e.emit_result("repl.stl", line.as_str());
-                                // eprintln!("{}", e.to_string().bright_red());
-                            }
-                        }
-
-                        if print_time {
-                            println!("Time taken: {:?}", now.elapsed());
-                        }
-
-                        // println!("Active Object Count: {:?}", steel::gc::OBJECT_COUNT);
+                        rt.block_on(action)
                     }
                 }
             }
