e7e9c54a48eb9bd2cea3357b75e579047d6cb157
mattwparas <matthewparas2020@u.northwestern.edu>
2021-03-12 22:34:26 -0600

    remove imports

--- examples/modules.rs
+++ examples/modules.rs
@@ -1,18 +1,14 @@
 use steel_vm::engine::Engine;
 
-// use env_logger::Builder;
-// use log::LevelFilter;
-
 // Steel allows for flexibility with modules as well. If you wanted to
 // expose a module with a clean api, you can do so using the `provide` syntax.
 // `require` is used to bring a module into scope - this requires the relative path
 // (from the directory of whatever project main is in), or absolute path to the script.
 // See the `modules` directory for the code used here
+//
+// Note: This should be run from the root of the project
+// Otherwise, it will fatal with a 'file not found'
 pub fn main() {
-    // let mut builder = Builder::new();
-
-    // builder.filter(Some("steel"), LevelFilter::Trace).init();
-
     let mut vm = Engine::new();
 
     vm.run(
