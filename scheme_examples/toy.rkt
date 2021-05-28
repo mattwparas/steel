966055a2b5db32ae6b04447c317e44976555161d
mattwparas <matthewparas2020@u.northwestern.edu>
2021-05-27 22:32:57 -0500

    some macro magic

--- scheme_examples/toy.rkt
+++ scheme_examples/toy.rkt
@@ -1,12 +1,47 @@
 ;; Sets of useful ideas for things
-(define (test-contract x)
-  (lambda (y)
-    (equal? (+ x y) 10)))
 
+(define-syntax match
+  (syntax-rules (list)
+    [(match list x) x]))
 
-(define/contract (foo x)
-  (->/c (test-contract 4) any/c)
-  (+ x 10))
+;; Do some pattern matching destructuring with this
+;; Pull in the let destruct macro and use that destructuring for lists
+;; (displayln (match list 10))
 
+(define-syntax destruct
+  (syntax-rules (list)
+    [(destruct ((list var) ret-value) body ...)
+     (let ((ret ret-value))
+       (destruct-list ((var) ret) body ...))]
+    [(destruct ((list var1 var2 ...) ret-value) body ...)
+     (let ((ret ret-value))
+       (destruct-list ((var1 var2 ...) ret) body ...))]))
 
-(displayln (foo 6))
+;; destruct a list
+;; maybe write a generic match library?
+(define-syntax destruct-list
+  (syntax-rules (..)
+    [(destruct-list ((var ..) ret-value) body ...)
+     (let ((var ret-value))
+       (begin body ...))]
+    ;; This should throw an error if there are remaining values in the list that
+    ;; the pattern matching isn't complete
+    [(destruct-list ((var) ret-value) body ...)
+     (let ((var (car ret-value)))
+       (begin body ...))]
+    [(destruct-list ((var1 var2 ...) ret-value) body ...)
+     (let ((var1 (car ret-value)))
+       (destruct-list ((var2 ...) (cdr ret-value))
+                     body ...))]))
+
+;; <var> without the <..> means bind the value there
+(destruct ((list a b c) '(1 2 3))
+          (displayln a)
+          (displayln b)
+          (displayln c))
+
+;; <var> .. means bind the rest of the list to that parameter
+(destruct ((list a b c ..) '(1 2 3 4 5))
+          (displayln a)
+          (displayln b)
+          (displayln c))
