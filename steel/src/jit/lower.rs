0e58be743532fe16712ab3bb04148b8731e56408
mattwparas <matthewparas2020@u.northwestern.edu>
2021-06-16 08:12:37 -0500

    some notes

--- steel/src/jit/lower.rs
+++ steel/src/jit/lower.rs
@@ -1,4 +1,12 @@
 use crate::parser::ast;
 use crate::parser::visitors::VisitorMut;
 
-pub struct MangleVars {}
+use im_rc::HashSet;
+
+// ((lambda (x y z) (+ x y z)) 10 20 30)
+// =>
+// (x = 10)
+// (y = 20)
+// (z = 30)
+// (+ x (+ y z))
+struct RenameScopedVars {}
