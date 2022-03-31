29a65fd525b67e9cb2f3f8c0164eca653663f1d9
mattwparas <matthewparas2020@u.northwestern.edu>
2022-03-30 17:06:06 -0700

    self hosted notes

--- self_hosted/contracts.rkt
+++ self_hosted/contracts.rkt
@@ -3,9 +3,19 @@
     (+ x y z))
 
 (define/contract (dummy foo bar)
-    (->/c string? integer? (->/c string? string?))
-    (list (int->string bar) foo))
+  (->/c string? integer? (->/c string? string?))
+  (list (int->string bar) foo))
 
+;; Function (Listof String)
+(define/contract (test lst)
+    (->/c (listof string?) (listof string?))
+    lst)
+
+;; Function(Any) -> Listof(Any)
+(lambda (x) (list x))
+
+;; Function(Any, Number) -> Listof( Any, Number)
+(lambda (x y) (list x (+ 1 y)))
 
 (lambda (x y z) (+ x y z)) ;; Function( (int, int, int) -> int )
 
@@ -17,6 +27,6 @@
 ;; continue to be the union of anything that gets added to it - in this case, 
 ;; list is called concretely with an int and any. Therefore the inferred type should be
 ;; List of string and any
-(lambda (foo bar) (list (int->string bar) foo)) 
+(lambda (foo bar) (list (int->string bar) foo))
 
 ; (applesauce 10 20 "hello world") ;; Type Error => "hello world" is a string, expected int
