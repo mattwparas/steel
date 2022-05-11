2f9c30527837c969853b84f37e9ac36896c1d723
mattwparas <matthewparas2020@u.northwestern.edu>
2022-05-11 14:13:30 -0700

    fix description

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -20,7 +20,7 @@ use log::LevelFilter;
 
 use clap::Parser;
 
-/// Simple program to greet a person
+/// Steel Interpreter Client
 #[derive(Parser, Debug)]
 #[clap(author, version, about, long_about = None)]
 struct Args {
