63a4e5b468bf8f1dbad718effe01c4517521bc79
mattwparas <matthewparas2020@u.northwestern.edu>
2022-11-06 18:09:00 -0800

    all tests passing

--- steel/tests/modules/a.rkt
+++ steel/tests/modules/a.rkt
@@ -1,17 +1,17 @@
-; (require "b.rkt")
+(require "b.rkt")
 
-; (provide a update! fetch-value capture-func)
+(provide a)
 
 ; (define *volatile* 0)
 
-; (define (a x) (a-private) (b 100))
-; (define (a-private) "hi this is a private function")
+(define (a x) (a-private) (b 100))
+(define (a-private) "hi this is a private function")
 ; (define (update!) 
 ;     (set! *volatile* (+ *volatile* 1)))
 
 ; (define (fetch-value) *volatile*)
 
-(provide capture-func)
+; (provide capture-func)
 
-(define (capture-func x)
-    (lambda (y) (+ x y)))
+; (define (capture-func x)
+;     (lambda (y) (+ x y)))
