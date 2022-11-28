b2a73d77e4e6dd1beaafac16e644b384e062d01a
mattwparas <matthewparas2020@u.northwestern.edu>
2022-11-27 21:21:05 -0800

    clean up

--- self_hosted/linq.rkt
+++
@@ -1,7 +0,0 @@
-;; Add an option to variable bindings that make it so they're frozen
-;; Attempting to set! a variable that has been tagged as immutable results in a compile time
-;; error
-(define test #:immutable 10)
-(define *appelsauce #:immutable 20)
-
-;; monday, tuesday, wednesday, thursday, friday
