d7fe1323248bfca4105180816fef609589f75ccd
mattwparas <matthewparas2020@u.northwestern.edu>
2023-01-31 07:38:35 -0800

    update toml files

--- crates/steel/build.rs
+++ crates/steel/build.rs
@@ -8,42 +8,35 @@ use steel_gen::generate_opcode_map;
 use steel_gen::OpCode::*;
 
 fn main() {
-    let out_dir = env::var_os("OUT_DIR").unwrap();
-    let dest_path = Path::new(&out_dir).join("dynamic.rs");
-    // fs::write(
-    //     &dest_path,
-    //     r#"
+    #[cfg(feature = "dynamic")]
+    {
+        let out_dir = env::var_os("OUT_DIR").unwrap();
+        let dest_path = Path::new(&out_dir).join("dynamic.rs");
 
-    //     pub fn message() -> &'static str {
-    //         println!("{:?}", OpCode::FUNC);
-    //         "Hello, World!"
-    //     }
-    //     "#,
-    // )
-    // .unwrap();
+        // TODO: Come up with better way for this to make it in
+        let patterns: Vec<Vec<(steel_gen::OpCode, usize)>> = vec![
+            vec![
+                (MOVEREADLOCAL0, 0),
+                (LOADINT2, 225),
+                (SUB, 2),
+                (CALLGLOBAL, 1),
+            ],
+            vec![(READLOCAL0, 0), (LOADINT1, 219), (SUB, 2), (CALLGLOBAL, 1)],
+            vec![(READLOCAL0, 0), (LOADINT2, 225), (LTE, 2), (IF, 7)],
+            vec![
+                (READLOCAL0, 0),
+                (LOADINT1, 219),
+                (SUB, 2),
+                (MOVEREADLOCAL0, 0),
+                (MOVEREADLOCAL1, 1),
+                (LOADINT1, 219),
+                (SUB, 2),
+                (CALLGLOBAL, 2),
+            ],
+        ];
 
-    // TODO: Come up with better way for this to make it in
-    let patterns: Vec<Vec<(steel_gen::OpCode, usize)>> = vec![
-        vec![
-            (MOVEREADLOCAL0, 0),
-            (LOADINT2, 225),
-            (SUB, 2),
-            (CALLGLOBAL, 1),
-        ],
-        vec![(READLOCAL0, 0), (LOADINT1, 219), (SUB, 2), (CALLGLOBAL, 1)],
-        vec![(READLOCAL0, 0), (LOADINT2, 225), (LTE, 2), (IF, 7)],
-        vec![
-            (READLOCAL0, 0),
-            (LOADINT1, 219),
-            (SUB, 2),
-            (MOVEREADLOCAL0, 0),
-            (MOVEREADLOCAL1, 1),
-            (LOADINT1, 219),
-            (SUB, 2),
-            (CALLGLOBAL, 2),
-        ],
-    ];
+        fs::write(dest_path, generate_opcode_map(patterns)).unwrap();
+    }
 
-    fs::write(dest_path, generate_opcode_map(patterns)).unwrap();
     println!("cargo:rerun-if-changed=build.rs");
 }
