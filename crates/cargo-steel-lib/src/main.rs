00173cd697d3a8aebeb399bdcdc9fcf4d1e67e3b
mattwparas <matthewparas2020@u.northwestern.edu>
2023-02-16 17:41:11 -0800

    add dylib installer

---
+++ crates/cargo-steel-lib/src/main.rs
@@ -0,0 +1,111 @@
+use std::{error::Error, path::PathBuf, process::Command};
+
+use cargo_metadata::{Message, MetadataCommand, Package};
+use std::process::Stdio;
+
+fn check_package_contains_dependency_on_steel(packages: &[Package]) -> Option<&Package> {
+    packages.iter().find(|x| x.name == "steel-core")
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    // let build_target = Command::new("cargo")
+    //     .arg("build")
+    //     .arg("--release")
+    //     .spawn()?
+    //     .wait()?
+    //     .success();
+
+    let mut steel_home = PathBuf::from(std::env::var("STEEL_HOME")?);
+
+    steel_home.push("native");
+
+    let metadata = MetadataCommand::new().exec()?;
+
+    // println!("{:#?}", metadata?.root_package());
+
+    // let package_name = metadata?.root_package().expect("Missing cargo toml!").
+
+    let package = match metadata.root_package() {
+        Some(p) => p,
+        None => return Err("cargo steel-lib must be run from within a crate".into()),
+    };
+
+    println!("Attempting to install: {:#?}", package.name);
+
+    if check_package_contains_dependency_on_steel(&metadata.packages).is_none() {
+        return Err(
+            "Cannot install package as a steel dylib - does not contain a dependency on steel!"
+                .into(),
+        );
+    }
+
+    let mut command = Command::new("cargo")
+        .args(&[
+            "build",
+            "--release",
+            "--message-format=json-render-diagnostics",
+        ])
+        .stdout(Stdio::piped())
+        .spawn()
+        .unwrap();
+
+    let reader = std::io::BufReader::new(command.stdout.take().unwrap());
+    let last = cargo_metadata::Message::parse_stream(reader)
+        .filter_map(|x| {
+            if let Ok(Message::CompilerArtifact(artifact)) = x {
+                Some(artifact)
+            } else {
+                None
+            }
+        })
+        .last()
+        .unwrap();
+
+    if last.target.kind == ["cdylib"] {
+        println!("Found a cdylib!");
+        // println!("{:#?}", last);
+
+        for file in last.filenames {
+            let filename = file.file_name().unwrap();
+
+            steel_home.push(filename);
+
+            println!("Copying {} to {}", file, &steel_home.to_str().unwrap());
+
+            std::fs::copy(file, &steel_home).unwrap();
+
+            steel_home.pop();
+        }
+    }
+
+    println!("Done!");
+
+    // {
+    //     match message.unwrap() {
+    //         Message::CompilerMessage(msg) => {
+    //             // println!("{:?}", msg);
+    //         }
+    //         Message::CompilerArtifact(artifact) => {
+    //             // println!("{:#?}", artifact.target);
+    //             if &artifact.target.name == package_name {
+    //                 println!("{:#?}", artifact);
+    //             }
+
+    //             // if artifact.target
+    //         }
+    //         Message::BuildScriptExecuted(script) => {
+    //             // println!("{:?}", script);
+    //         }
+    //         Message::BuildFinished(finished) => {
+    //             // println!("{:?}", finished);
+    //         }
+    //         _ => (), // Unknown message
+    //     }
+    // }
+
+    command.wait().expect("Couldn't get cargo's exit status");
+
+    Ok(())
+
+    // println!("Hello, world!");
+}
