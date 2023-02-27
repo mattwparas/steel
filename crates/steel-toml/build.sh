71a3f233784064b7faa2a892e4ab1c1817fad45a
mattwparas <matthewparas2020@u.northwestern.edu>
2023-02-26 21:02:14 -0800

    remove bash scripts

--- crates/steel-toml/build.sh
+++
@@ -1,5 +0,0 @@
-cargo build --release
-cd ../../
-cp target/release/libsteel_toml.so $STEEL_HOME/native/
-cp target/release/libsteel_toml.dylib $STEEL_HOME/native/
-cp target/release/libsteel_toml.d $STEEL_HOME/native/
--- crates/steel-webserver/build.sh
+++
@@ -1,5 +0,0 @@
-cargo build --release
-cd ../../
-cp target/release/libsteel_webserver.so $STEEL_HOME/native/
-cp target/release/libsteel_webserver.dylib $STEEL_HOME/native/
-cp target/release/libsteel_webserver.d $STEEL_HOME/native/
