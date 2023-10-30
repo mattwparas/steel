0216efaae47ac12e8461542284a364623c478491
mattwparas <matthewparas2020@u.northwestern.edu>
2023-10-29 21:53:08 -0700

    enable env logger

--- src/main.rs
+++ src/main.rs
@@ -4,25 +4,25 @@ use clap::Parser;
 use steel_interpreter::Args;
 
 fn main() -> Result<(), Box<dyn Error>> {
-    // env_logger::init();
+    env_logger::init();
 
-    let mut builder = env_logger::Builder::new();
+    // let mut builder = env_logger::Builder::new();
 
-    let log_targets = [
-        // "requires",
-        // "modules",
-        "gc",
-        // "kernel",
-        // "super-instructions",
-        // "lambda-lifting",
-        // "dylibs",
-    ];
+    // let log_targets = [
+    // "requires",
+    // "modules",
+    // "gc",
+    // "kernel",
+    // "super-instructions",
+    // "lambda-lifting",
+    // "dylibs",
+    // ];
 
-    for target in log_targets {
-        builder.filter(Some(target), log::LevelFilter::Trace);
-    }
+    // for target in log_targets {
+    // builder.filter(Some(target), log::LevelFilter::Trace);
+    // }
 
-    builder.init();
+    // builder.init();
 
     let clap_args = Args::parse();
 
