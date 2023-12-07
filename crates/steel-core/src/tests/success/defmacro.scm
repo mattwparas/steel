0bc1db69bdc15416b2a8bcabb9a6e5934f678a8c
mattwparas <matthewparas2020@u.northwestern.edu>
2023-12-06 20:31:09 -0800

    add more to test

--- crates/steel-core/src/tests/success/defmacro.scm
+++ crates/steel-core/src/tests/success/defmacro.scm
@@ -1,5 +1,14 @@
+(begin-for-syntax
+  (define (my-function x)
+    (displayln x)))
+
 (defmacro (leave-alone x) (cadr (syntax-e x)))
 
 (leave-alone (define foo-bar 100))
 
+(defmacro (print-value x) (my-function x) (cadr (syntax-e x)))
+
+(print-value (define baz 100))
+
 (assert! (equal? foo-bar 100))
+(assert! (equal? baz 100))
