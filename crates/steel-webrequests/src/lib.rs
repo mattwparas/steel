7bd162ef4d678d616c8b2cc16325cf99fb397df5
mattwparas <matthewparas2020@u.northwestern.edu>
2024-01-20 20:44:10 -0800

    add more methods to web request dylib

--- crates/steel-webrequests/src/lib.rs
+++ crates/steel-webrequests/src/lib.rs
@@ -13,21 +13,30 @@ fn create_module() -> FFIModule {
 
     module
         .register_fn("get", get)
+        .register_fn("post", post)
+        .register_fn("put", put)
+        .register_fn("delete", delete)
+        .register_fn("patch", patch)
+        .register_fn("head", head)
+        .register_fn("set-header!", BlockingRequest::set)
+        .register_fn("set-query-parameter!", BlockingRequest::query)
+        .register_fn("set-timeout/ms!", BlockingRequest::timeout_ms)
         .register_fn(
             "call",
             |request: BlockingRequest| -> Result<SteelResponse, BlockingError> {
-                Request::call(request.0)
+                Request::call(request.0.unwrap())
                     .map(|x| x.into())
                     .map_err(BlockingError::Ureq)
             },
         )
+        .register_fn("call-with-json", BlockingRequest::call_with_json)
         .register_fn("response->text", SteelResponse::into_text);
 
     module
 }
 
 #[derive(Clone)]
-struct BlockingRequest(Request);
+struct BlockingRequest(Option<Request>);
 struct BlockingResponse(Response);
 
 enum BlockingError {
@@ -40,7 +49,53 @@ impl Custom for BlockingResponse {}
 impl Custom for BlockingError {}
 
 fn get(url: String) -> BlockingRequest {
-    BlockingRequest(ureq::get(&url))
+    BlockingRequest(Some(ureq::get(&url)))
+}
+
+fn post(url: String) -> BlockingRequest {
+    BlockingRequest(Some(ureq::post(&url)))
+}
+
+fn put(url: String) -> BlockingRequest {
+    BlockingRequest(Some(ureq::put(&url)))
+}
+
+fn delete(url: String) -> BlockingRequest {
+    BlockingRequest(Some(ureq::delete(&url)))
+}
+
+fn patch(url: String) -> BlockingRequest {
+    BlockingRequest(Some(ureq::patch(&url)))
+}
+
+fn head(url: String) -> BlockingRequest {
+    BlockingRequest(Some(ureq::head(&url)))
+}
+
+impl BlockingRequest {
+    fn query(&mut self, parameter: String, value: String) {
+        self.0 = Some(self.0.take().unwrap().query(&parameter, &value));
+    }
+
+    fn set(&mut self, header: String, value: String) {
+        self.0 = Some(self.0.take().unwrap().set(&header, &value));
+    }
+
+    // TODO: Add FFI conversion form u64 as well
+    fn timeout_ms(&mut self, time_in_ms: usize) {
+        self.0 = Some(
+            self.0
+                .take()
+                .unwrap()
+                .timeout(std::time::Duration::from_millis(time_in_ms as u64)),
+        );
+    }
+
+    fn call_with_json(&mut self, json: String) -> Result<SteelResponse, BlockingError> {
+        Request::send_json(self.0.clone().unwrap(), json)
+            .map(|x| x.into())
+            .map_err(BlockingError::Ureq)
+    }
 }
 
 struct SteelResponse {
