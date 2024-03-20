b09a555f0e0c5e316c0ad7e27aeda5c8cf8026de
Constantin Gahr <constantin.gahr@ipp.mpg.de>
2024-03-20 16:54:24 +0100

    provide cargo-steel-lib correctly during installation

--- flake.nix
+++ flake.nix
@@ -22,11 +22,12 @@
 
       manifest = pkgs.lib.importTOML ./Cargo.toml;
       steel = with pkgs;
-        rustPlatform.buildRustPackage rec {
+        rustPlatform.buildRustPackage {
           pname = manifest.package.name;
           version = manifest.workspace.package.version;
           src = gitignoreSource ./.;
           cargoLock.lockFile = ./Cargo.lock;
+          cargoBuildFlags = "-p cargo-steel-lib -p steel-interpreter";
           buildInputs = [openssl] ++ lib.optionals stdenv.isDarwin [darwin.apple_sdk.frameworks.Security];
           nativeBuildInputs = [
             pkg-config
@@ -34,11 +35,13 @@
           # Test failing
           doCheck = false;
           postInstall = ''
+            substituteInPlace /build/source/cogs/installer/download.scm --replace-warn "cargo-steel-lib" "$out/bin/cargo-steel-lib"
             mkdir $out/lib
             export STEEL_HOME="$out/lib"
             pushd cogs
             $out/bin/steel install.scm
             popd
+            rm "$out/bin/cargo-steel-lib"
           '';
         };
     in rec {
