cc5623aa8f0d0e90f61d935883347cae015caf09
Matt Paras <matthewparas2020@u.northwestern.edu>
2024-08-15 21:01:03 -0700

    rework random module

--- crates/steel-core/src/primitives/random.rs
+++ crates/steel-core/src/primitives/random.rs
@@ -1,38 +1,36 @@
 use rand::prelude::*;
 
-use crate::steel_vm::builtin::DocTemplate;
 use crate::steel_vm::register_fn::RegisterFn;
 use crate::{rvals::Custom, steel_vm::builtin::BuiltInModule};
 
 impl Custom for ThreadRng {}
 
-pub(crate) const THREAD_RNG_DOC: DocTemplate<'static> = DocTemplate {
-    signature: "(thread-rng!) -> ThreadRng?",
-    params: &[],
-    description: r#"Constructs a `ThreadRng` object"#,
-    examples: &[],
-};
+// pub(crate) const THREAD_RNG_DOC: DocTemplate<'static> = DocTemplate {
+//     signature: "(thread-rng!) -> ThreadRng?",
+//     params: &[],
+//     description: r#"Constructs a `ThreadRng` object"#,
+//     examples: &[],
+// };
 
-pub(crate) const RNG_GEN_USIZE: DocTemplate<'static> = DocTemplate {
-    signature: "(rng->gen-usize rng) -> int?",
-    params: &["rng: ThreadRng?"],
-    description: r#"Generates a random unsigned integer"#,
-    examples: &[],
-};
+// pub(crate) const RNG_GEN_USIZE: DocTemplate<'static> = DocTemplate {
+//     signature: "(rng->gen-usize rng) -> int?",
+//     params: &["rng: ThreadRng?"],
+//     description: r#"Generates a random unsigned integer"#,
+//     examples: &[],
+// };
 
 /// Constructs the random module
 pub fn random_module() -> BuiltInModule {
     let mut module = BuiltInModule::new("steel/random");
 
-    // module
-    // .register_fn("thread-rng!", thread_rng)
-    // .register_doc("thread-rng!", THREAD_RNG_DOC)
-    // .register_fn("rng->gen-usize", ThreadRng::gen::<usize>)
-    // .register_doc("rng->gen-usize", RNG_GEN_USIZE)
-    // .register_fn(
-    //     "rng->gen-range",
-    //     |rng: &mut ThreadRng, x: isize, y: isize| ThreadRng::gen_range(rng, x..y),
-    // );
+    module
+        // .register_fn("thread-rng!", thread_rng)
+        // .register_doc("thread-rng!", THREAD_RNG_DOC)
+        .register_fn("rng->gen-usize", || thread_rng().gen::<usize>())
+        // .register_doc("rng->gen-usize", RNG_GEN_USIZE)
+        .register_fn("rng->gen-range", |x: isize, y: isize| {
+            thread_rng().gen_range(x..y)
+        });
 
     module
 }
