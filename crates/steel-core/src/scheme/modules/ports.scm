35ee4dc2b2a38124a1dec6c8469fe91a51fe845e
may <m4rch3n1ng@gmail.com>
2025-03-21 22:47:25 +0100

    move other port functions into builtin steel/ports module

--- crates/steel-core/src/scheme/modules/ports.scm
+++ crates/steel-core/src/scheme/modules/ports.scm
@@ -1,8 +1,11 @@
 (require-builtin steel/base)
+(require "#%private/steel/control")
 
 (provide call-with-port
          call-with-output-file
-         call-with-input-file)
+         call-with-input-file
+         call-with-output-string
+         with-output-to-string)
 
 (define (call-with-port port proc)
   (let ([ret (proc port)])
@@ -14,3 +17,13 @@
 
 (define (call-with-input-file file proc)
   (call-with-port (open-input-file file) proc))
+
+(define (call-with-output-string proc)
+  (define output-string (open-output-string))
+  (proc output-string)
+  (get-output-string output-string))
+
+(define (with-output-to-string proc)
+  (call-with-output-string (lambda (p)
+                             (parameterize ([current-output-port p])
+                               (proc)))))
