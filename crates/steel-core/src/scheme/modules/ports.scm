02916c265e802ec38e82d0a64ca4654c0418cb59
may <m4rch3n1ng@gmail.com>
2025-03-30 18:39:08 +0200

    implement with-output-to-file and with-input-from-file

--- crates/steel-core/src/scheme/modules/ports.scm
+++ crates/steel-core/src/scheme/modules/ports.scm
@@ -4,6 +4,8 @@
 (provide call-with-port
          call-with-output-file
          call-with-input-file
+         with-output-to-file
+         with-input-from-file
          call-with-output-string
          call-with-input-string
          with-output-to-string
@@ -45,6 +47,33 @@
   (call-with-port (open-input-file file) proc))
 
 ;;@doc
+;; Similar to `call-with-output-file`, but installs the newly opened port as the `current-output-port` instead of passing it as an argument.
+;; If *thunk* returns, then the temporary port will be closed and the return value of *thunk* returned.
+;;
+;; (with-output-to-file file proc) > any/c
+;;
+;; - file : string?
+;; - thunk : procedure?
+(define (with-output-to-file file thunk)
+  (call-with-output-file file
+                         (lambda (port)
+                           (parameterize ([current-output-port port])
+                             (thunk)))))
+
+;;@doc
+;; Similar to `call-with-input-file`, but installs the newly opened port as the `current-input-port` instead of passing it as an argument.
+;; If *thunk* returns, then the temporary port will be closed and the return value of *thunk* returned.
+;;
+;; (with-input-from-file file proc) > any/c
+;;
+;; - file : string?
+;; - thunk : procedure?
+(define (with-input-from-file file thunk)
+  (call-with-input-file file
+                           (lambda (port)
+                             (parameterize ([current-input-port port])
+                               (thunk)))))
+
 ;; Calls the given *proc* with an output string port created with `open-output-string`.
 ;; If *proc* returns, then the content from the string port will be returned.
 ;;
@@ -66,13 +95,28 @@
 (define (call-with-input-string string proc)
   (call-with-port (open-input-string string) proc))
 
-(define (with-output-to-string proc)
+;;@doc
+;; Similar to `call-with-output-string`, but installs the newly opened port as the `current-input-port` instead of passing it as an argument.
+;; If *thunk* returns, then the content from the string port will be returned.
+;;
+;; (with-output-to-string thunk) -> string?
+;;
+;; - thunk : procedure?
+(define (with-output-to-string thunk)
   (call-with-output-string (lambda (port)
                              (parameterize ([current-output-port port])
-                               (proc)))))
+                               (thunk)))))
 
-(define (with-input-from-string string proc)
+;;@doc
+;; Similar to `call-with-output-string`, but installs the newly opened port as the `current-input-port` instead of passing it as an argument.
+;; If *thunk* returns, then the return value of *thunk* returned.
+;;
+;; (with-input-from-string string thunk) -> any/c
+;;
+;; - string : string?
+;; - thunk : procedure?
+(define (with-input-from-string string thunk)
   (call-with-input-string string
                           (lambda (port)
                             (parameterize ([current-input-port port])
-                              (proc)))))
+                              (thunk)))))
