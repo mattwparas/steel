d45699a3af54aaa92293fb8a9b4add5b4a3fdfaa
Evgeny Fomin <fominok@hotmail.com>
2025-04-21 09:22:48 +0200

    add repl builder

--- crates/steel-repl/src/lib.rs
+++ crates/steel-repl/src/lib.rs
@@ -2,6 +2,9 @@
 mod repl;
 mod highlight;
 
+pub use colored;
+pub use repl::Repl;
+
 /// Run the Steel repl with the given `Engine`. Exits on IO error or when the user requests to exit.
 pub fn run_repl(vm: steel::steel_vm::engine::Engine) -> std::io::Result<()> {
     repl::repl_base(vm)
