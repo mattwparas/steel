4c33cdb36a2c94bdceeee05b0231f3216f86398c
Nik Revenco <154856872+NikitaRevenco@users.noreply.github.com>
2025-05-12 23:21:32 +0100

    chore: more clarifications

--- docs/src/examples/variables.scm
+++ docs/src/examples/variables.scm
@@ -8,19 +8,24 @@
 ; The first argument is `1`. The second argument is `2`
 (define three (+ 1 2))
 
-; functions that end with an exlamation mark `!` are macros.
+(define actual (to-string "1 + 2 =" three))
+(define expected "1 + 2 = 3")
+
+; Functions that end with a question mark `?` are predicates.
+; They return a boolean: `#true` or `#false`
+;
+; The `eq?` predicate returns `#true` if the 2 arguments are
+; equal, and `#false` otherwise
+(define result (eq? (actual expected)))
+
+; Functions that end with an exclamation mark `!` are macros.
+;
+; Macros expand to code.
 ;
 ; The `assert!` macro throws an error if its argument is `#false`
-;
-; Macros do not evaluate to anything.
-; Rather, they expand to more code
-;
-; `eq?`: functions that return a boolean:
-; `#true` (alias `#t`) or `#false` (alias `#f`)
-; end with a question mark `?
-(assert! (eq? (to-string "1 + 2 =" three "1 + 2 = 3")))
+(assert! result)
 
-; boolean operators
-(assert! (and #t #t))
-(assert! (or #t #f))
-(assert! (not #f))
+; Boolean operators
+(assert! (and #true #true))
+(assert! (or #true #false))
+(assert! (not #false))
