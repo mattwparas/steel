7538766d3f3d3c926c3930c73de901d584bed799
Matt Paras <matthewparas2020@u.northwestern.edu>
2025-08-02 14:23:06 -0700

    clean up warnings

--- crates/steel-core/src/compiler/constants.rs
+++ crates/steel-core/src/compiler/constants.rs
@@ -1,4 +1,8 @@
-use crate::gc::shared::{MutContainer, ShareableMut};
+use crate::gc::shared::MutContainer;
+
+#[cfg(not(feature = "triomphe"))]
+use crate::gc::shared::ShareableMut;
+
 use crate::gc::{Shared, SharedMut};
 use crate::parser::tryfrom_visitor::TryFromExprKindForSteelVal;
 use crate::rvals::{into_serializable_value, Result, SerializableSteelVal, SteelVal};
