75deeb267bd51587b614f7032484cb3b7bdbaa73
Matt Paras <matthewparas2020@u.northwestern.edu>
2025-08-18 10:43:32 -0700

    another benchmark

--- crates/steel-core/src/values/port.rs
+++ crates/steel-core/src/values/port.rs
@@ -353,7 +353,7 @@ impl SteelPortRepr {
 
         let s = c.encode_utf8(&mut buf);
 
-        let _ = self.write(s.as_bytes())?;
+        let _ = self.write_without_flush(s.as_bytes())?;
 
         Ok(())
     }
@@ -433,6 +433,35 @@ impl SteelPortRepr {
             ($br: expr) => {{
                 let result = $br.write(buf)?;
                 $br.flush()?;
+                result
+            }};
+        );
+
+        let result = match self {
+            SteelPortRepr::FileOutput(_, writer) => write_and_flush![writer],
+            SteelPortRepr::StdOutput(writer) => write_and_flush![writer],
+            SteelPortRepr::StdError(writer) => write_and_flush![writer],
+            SteelPortRepr::ChildStdInput(writer) => write_and_flush![writer],
+            SteelPortRepr::StringOutput(writer) => write_and_flush![writer],
+            SteelPortRepr::DynWriter(writer) => write_and_flush![writer.lock().unwrap()],
+            // TODO: Should tcp streams be both input and output ports?
+            SteelPortRepr::TcpStream(tcp) => tcp.write(buf)?,
+            SteelPortRepr::FileInput(_, _)
+            | SteelPortRepr::StdInput(_)
+            | SteelPortRepr::DynReader(_)
+            | SteelPortRepr::ChildStdOutput(_)
+            | SteelPortRepr::ChildStdError(_)
+            | SteelPortRepr::StringInput(_) => stop!(ContractViolation => "expected output-port?"),
+            SteelPortRepr::Closed => stop!(Io => "port is closed"),
+        };
+
+        Ok(result)
+    }
+
+    pub fn write_without_flush(&mut self, buf: &[u8]) -> Result<usize> {
+        macro_rules! write_and_flush(
+            ($br: expr) => {{
+                let result = $br.write(buf)?;
                 result
             }};
         );
