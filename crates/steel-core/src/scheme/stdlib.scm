15dbda870dca6973ba8f4d9e05d70f81fe721682
may <63159454+m4rch3n1ng@users.noreply.github.com>
2025-08-25 04:03:42 +0200

    fix strange behaviour of (min) and (max) (#484)

--- crates/steel-core/src/scheme/stdlib.scm
+++ crates/steel-core/src/scheme/stdlib.scm
@@ -563,9 +563,11 @@
 (define fold (lambda (f a l) (foldl f a l)))
 (define reduce (lambda (f a l) (fold f a l)))
 
-(define max (lambda (x . num-list) (fold (lambda (y z) (if (> y z) y z)) x (cons 0 num-list))))
-(define min
-  (lambda (x . num-list) (fold (lambda (y z) (if (< y z) y z)) x (cons 536870911 num-list))))
+(define (max x . rest)
+  (fold (lambda (y z) (if (> y z) y z)) x rest))
+
+(define (min x . rest)
+  (fold (lambda (y z) (if (< y z) y z)) x rest))
 
 (define mem-helper
   (lambda (pred op) (lambda (acc next) (if (and (not acc) (pred (op next))) next acc))))
