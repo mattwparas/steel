bee28d79c54bcb0b72db5300bfb34bd692666edd
may <m4rch3n1ng@gmail.com>
2025-09-30 19:27:21 +0200

    adjust and add some docs to the string->symbol procedures

--- crates/steel-core/src/primitives/strings.rs
+++ crates/steel-core/src/primitives/strings.rs
@@ -498,19 +498,30 @@ pub fn to_string(args: &[SteelVal]) -> Result<SteelVal> {
     Ok(SteelVal::StringV(error_message.into()))
 }
 
+/// Return an uninterned symbol from the given string.
+///
+/// (string->uninterned-symbol string?) -> symbol?
+///
+/// # Examples
+///
+/// ```scheme
+/// (string->uninterned-symbol "abc") ;; => 'abc
+/// (string->uninterned-symbol "pea pod") ;; => '|pea pod|
+/// ```
 #[function(name = "string->uninterned-symbol", constant = true)]
 pub fn string_to_uninterned_symbol(value: SteelString) -> SteelVal {
     SteelVal::SymbolV(value)
 }
 
-/// Converts a string into a symbol.
+/// Returns an interned symbol from the given string.
 ///
 /// (string->symbol string?) -> symbol?
 ///
 /// # Examples
 ///
 /// ```scheme
-/// > (string->symbol "FooBar") ;; => 'FooBar
+/// > (string->symbol "abc") ;; => 'abc
+/// > (string->symbol "pea pod") ;; => '|pea pod|
 /// ```
 #[steel_derive::context(name = "string->symbol", constant = true, arity = "Exact(1)")]
 pub fn string_to_symbol(ctx: &mut VmCore, args: &[SteelVal]) -> Option<Result<SteelVal>> {
