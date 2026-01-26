a6ff9d781fb176032dbc32e7713eff32baa4f6c2
Matt Paras <matthewparas2020@u.northwestern.edu>
2026-01-26 10:44:50 -0800

    gracefully join on the thread when we're done with the interrupt handler

--- crates/steel-core/src/steel_vm/interrupt.rs
+++ crates/steel-core/src/steel_vm/interrupt.rs
@@ -1,5 +1,8 @@
 use std::{
-    sync::{atomic::AtomicBool, Arc},
+    sync::{
+        atomic::{AtomicBool, Ordering},
+        Arc,
+    },
     time::Duration,
 };
 
@@ -10,6 +13,13 @@ pub struct InterruptHandler {
     running: Arc<AtomicBool>,
     handle: std::thread::JoinHandle<()>,
     done: crossbeam_channel::Sender<()>,
+    dropped: Arc<AtomicBool>,
+}
+
+impl Drop for InterruptHandler {
+    fn drop(&mut self) {
+        self.dropped.store(true, Ordering::Relaxed);
+    }
 }
 
 impl InterruptHandler {
@@ -19,33 +29,34 @@ impl InterruptHandler {
 
         let controller_clone = controller.clone();
         let running_clone = running.clone();
+        let dropped = Arc::new(AtomicBool::new(false));
+
+        let dropped_clone = dropped.clone();
 
         let (sender, receiver) = crossbeam_channel::bounded::<()>(16);
 
         let thread_handle = std::thread::spawn(move || {
             let controller = controller_clone;
             let running = running_clone;
+            let dropped = dropped_clone;
 
-            // TODO: Don't just want an infinite loop, also figure out a way to
-            // kill this thread.
-            loop {
+            while !dropped.load(std::sync::atomic::Ordering::Relaxed) {
                 std::thread::park();
 
                 while running.load(std::sync::atomic::Ordering::Relaxed) {
                     // Either, sleep or receive event. Whichever comes first.
-
                     match receiver.recv_timeout(timeout) {
-                        Ok(_) => break,
+                        Ok(_) => {}
                         Err(_) => {
                             controller.interrupt();
-
                             while receiver.try_recv().is_ok() {
                                 // Drain the messages
                                 // for the next batch
                             }
-                            break;
                         }
                     }
+
+                    break;
                 }
             }
         });
@@ -55,6 +66,7 @@ impl InterruptHandler {
             running,
             handle: thread_handle,
             done: sender,
+            dropped,
         }
     }
 
