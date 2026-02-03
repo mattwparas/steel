134057adbbfcae7145d624182ead8f09154a0279
Matt Paras <matthewparas2020@u.northwestern.edu>
2026-02-03 08:00:32 -0800

    checkpoint

---
+++ cap.scm
@@ -0,0 +1,3 @@
+(require (capabilities-in (file-system-access (fs/read) ".") "read.scm"))
+
+(reading-test)
---
+++ read.scm
@@ -0,0 +1,6 @@
+(provide reading-test)
+
+(displayln (read-dir "../"))
+
+(define (reading-test)
+  (read-dir "../"))
