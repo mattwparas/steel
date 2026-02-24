74b79b7c6fc0e47c98164f997437ccd340c8209d
Matthew Paras <34500476+mattwparas@users.noreply.github.com>
2026-02-24 08:39:17 -0800

    Rework kernel expansion just a tad (#631)
    * rework kernel expansion just a tad
    
    * fix build
    
    * fix compilation across multiple threads with the same bytecode

--- crates/steel-core/src/jit2/cgen.rs
+++ crates/steel-core/src/jit2/cgen.rs
@@ -978,7 +978,23 @@ impl JIT {
     ) -> Result<*const u8, String> {
         // self.ctx.set_disasm(true);
 
+        if let Some(data) = self.module.get_name(&name) {
+            match data {
+                cranelift_module::FuncOrDataId::Func(func_id) => {
+                    return Ok(self.module.get_finalized_function(func_id));
+                }
+                cranelift_module::FuncOrDataId::Data(data_id) => panic!(),
+            }
+        }
+
         let (params, stmts) = (Default::default(), instructions);
+
+        let pointer = self.module.target_config().pointer_type();
+
+        let mut param = AbiParam::new(pointer);
+        param.purpose = ArgumentPurpose::VMContext;
+
+        self.ctx.func.signature.params.push(param);
 
         let id = self
             .module
@@ -999,7 +1015,7 @@ impl JIT {
 
         if let Err(e) = cranelift::codegen::verify_function(&self.ctx.func, self.module.isa()) {
             // println!("{:#?}", self.ctx.func);
-            println!("{:#?}", e);
+            eprintln!("{:#?}", e);
             self.module.clear_context(&mut self.ctx);
             return Err(format!("errors: {:#?}", e));
         }
@@ -1007,7 +1023,8 @@ impl JIT {
         self.module
             .define_function(id, &mut self.ctx)
             .map_err(|e| {
-                println!("error in defining function");
+                eprintln!("error in defining function: {}", e);
+                self.module.clear_context(&mut self.ctx);
                 e.to_string()
             })?;
 
@@ -1066,14 +1083,6 @@ impl JIT {
 
         // Upgrade to 128 bit?
         let int = Type::int(128).unwrap();
-
-        // Set up pointer type to be the first argument.
-        let pointer = self.module.target_config().pointer_type();
-
-        let mut param = AbiParam::new(pointer);
-        param.purpose = ArgumentPurpose::VMContext;
-
-        self.ctx.func.signature.params.push(param);
 
         // dbg!(&self.ctx.func.signature);
 
