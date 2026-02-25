78070ce4339336708abcb30642a1ef634ac5a877
Matt Paras <matthewparas2020@u.northwestern.edu>
2026-02-25 15:33:34 -0800

    make these things available in the kernel

--- crates/steel-core/src/scheme/kernel.scm
+++ crates/steel-core/src/scheme/kernel.scm
@@ -12,6 +12,10 @@
 ; (define #%syntax-bindings (make-parameter (hash)))
 ; (define #%syntax-binding-kind (make-parameter (hash)))
 
+(require-builtin steel/base)
+(require "#%private/steel/control")
+(require "#%private/steel/ports")
+
 (set! #%syntax-bindings (make-parameter (hash)))
 (set! #%syntax-binding-kind (make-parameter (hash)))
 
@@ -564,3 +568,98 @@
                                 (%proto-hash-insert% ,(string->symbol env) (quote ,name) ,func)))
                    (register-macro-transformer! name env)))
  'void)
+
+(require-builtin #%private/steel/reader as reader.)
+
+(define *reader* (reader.new-reader))
+
+(define (read . port)
+  (if (null? port)
+      (reader.#%intern (read-impl reader.reader-read-one))
+      (parameterize ([current-input-port (car port)])
+        (reader.#%intern (read-impl reader.reader-read-one)))))
+
+(define (read-syntax-object . port)
+  (if (null? port)
+      (read-impl reader.reader-read-one-syntax-object)
+      (parameterize ([current-input-port (car port)])
+        (read-impl reader.reader-read-one-syntax-object))))
+
+(define (read-impl finisher)
+  (cond
+    [(reader.reader-empty? *reader*)
+
+     (if (or (#%string-input-port? (current-input-port)) (#%file-input-port? (current-input-port)))
+         (begin
+
+           (define entry (read-port-to-string (current-input-port)))
+           (reader.reader-push-string *reader* entry)
+
+           (let ([next (finisher *reader*)])
+             ; (displayln next)
+             (if (void? next)
+                 (begin
+                   (let ([maybe-next-line (read-line-from-port (current-input-port))])
+                     (if (eof-object? maybe-next-line)
+                         (if (or (#%string-input-port? (current-input-port))
+                                 (#%file-input-port? (current-input-port)))
+                             maybe-next-line
+                             (begin
+                               (set! *reader* (reader.new-reader))
+                               (error "missing closing paren - unexpected eof")))
+                         ;; If the next line is not empty,
+                         (begin
+                           (reader.reader-push-string *reader* maybe-next-line)
+                           (read-impl finisher)))))
+
+                 next)))
+
+         (begin
+
+           (define next-line (read-line-from-port (current-input-port)))
+           (cond
+             [(string? next-line)
+              (reader.reader-push-string *reader* next-line)
+
+              (let ([next (finisher *reader*)])
+                (if (void? next)
+                    (begin
+                      (let ([maybe-next-line (read-line-from-port (current-input-port))])
+                        (if (eof-object? maybe-next-line)
+                            (if (or (#%string-input-port? (current-input-port))
+                                    (#%file-input-port? (current-input-port)))
+                                maybe-next-line
+                                (begin
+                                  (set! *reader* (reader.new-reader))
+                                  (error "missing closing paren - unexpected eof")))
+                            ;; If the next line is not empty,
+                            (begin
+                              (reader.reader-push-string *reader* maybe-next-line)
+                              (read-impl finisher)))))
+                    next))]
+
+             [else next-line])))]
+
+    ;; The reader is not empty!
+    [else
+     (let ([next (reader.reader-read-one *reader*)])
+
+       (if (void? next)
+           ;; TODO: Share this code with the above
+           (begin
+             (let ([maybe-next-line (read-line-from-port (current-input-port))])
+               (if (eof-object? maybe-next-line)
+                   ;; Stupid hack
+                   (if (or (#%string-input-port? (current-input-port))
+                           (#%file-input-port? (current-input-port)))
+                       maybe-next-line
+                       (begin
+                         ;; TODO: drain the reader - consider a separate function for this
+                         (set! *reader* (reader.new-reader))
+                         (error "missing closing paren - unexpected eof")))
+                   ;; If the next line is not empty,
+                   (begin
+                     (reader.reader-push-string *reader* maybe-next-line)
+                     (read-impl finisher)))))
+
+           next))]))
