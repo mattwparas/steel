638c902f1efbc1a21761e8a9f2d4a6c33c1029df
Matt Paras <matthewparas2020@u.northwestern.edu>
2026-03-01 22:12:55 -0800

    remove erroneous check

--- crates/steel-core/src/scheme/stdlib.scm
+++ crates/steel-core/src/scheme/stdlib.scm
@@ -1167,9 +1167,7 @@
       (#%for-each func2 (cdr lst2))))
   (if (function? func)
       (if (list? lst)
-          (unless (null? lst)
-            (func (car lst))
-            (#%for-each func (cdr lst)))
+          (#%for-each func (cdr lst))
           (error-with-span (current-function-span) "for-each expected a list, found: " lst))
       (error-with-span (current-function-span) "for-each expected a function, found: " func)))
 
