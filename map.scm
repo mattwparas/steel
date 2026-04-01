9fc9f8276403a1dcdf4ca55e27f8870b21f2d229
Matt Paras <matthewparas2020@u.northwestern.edu>
2026-04-01 07:56:49 -0700

    include map for testing

---
+++ map.scm
@@ -0,0 +1,40 @@
+(provide map2)
+
+(define (map2 function list1 . more-lists)
+  (define (some? function list)
+    (and (pair? list) (or (function (car list)) (some? function (cdr list)))))
+
+  (define (map1 func accum lst)
+    (if (null? lst) (reverse accum) (map1 func (cons (func (car lst)) accum) (cdr lst))))
+
+  (define (map-many func accum lsts)
+    (if (some? null? lsts)
+        (reverse accum)
+        (map-many func (cons (apply func (map1 car '() lsts)) accum) (map1 cdr '() lsts))))
+
+  (if (null? more-lists)
+      (map1 function '() list1)
+      (let ([lists (cons list1 more-lists)])
+        (if (some? null? lists) '() (map-many function '() lists)))))
+
+; ; (define (map1 func accum lst)
+; ;   (if (null? lst) (reverse accum) (map1 func (cons (func (car lst)) accum) (cdr lst))))
+
+; ; ; (provide map-new)
+; ; (define (map-new func lst)
+; ;   (map1 func '() lst))
+
+(define r (range 0 100000))
+
+(define results (mutable-vector))
+
+(define (loop x)
+  (if (= x 100)
+      void
+      (begin
+        (vector-push! results (map (lambda (x) (+ x 1)) r))
+        (loop (+ x 1)))))
+
+(provide run)
+(define (run)
+  (loop 0))
